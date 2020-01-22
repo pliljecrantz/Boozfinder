8302aed654b7876840cea3abcd2558adca79ab3a
pliljecrantz <p.liljecrantz@gmail.com>
2020-01-22 20:33:55 +0100

    Added Swagger

--- Boozfinder/Startup.cs
+++ Boozfinder/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace Boozfinder
 {
@@ -26,6 +30,29 @@ namespace Boozfinder
             services.AddScoped<IUserProvider, UserProvider>();
             services.AddScoped<IAuthenticationProvider, AuthenticationProvider>();
             services.AddScoped<IBoozeProvider, BoozeProvider>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Boozfinder API",
+                    //Description = "A simple API for Boozfinder",
+                    //TermsOfService = new Uri("https://example.com/terms"),
+                    //Contact = new OpenApiContact
+                    //{
+                    //    Name = "My name",
+                    //    Email = string.Empty,
+                    //    Url = new Uri("https://twitter.com/example"),
+                    //},
+                    //License = new OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +64,16 @@ namespace Boozfinder
             }
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Boozfinder API v1");
+            });
         }
     }
 }
